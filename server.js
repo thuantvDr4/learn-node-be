741988b31f87cdc991405cc5fdd07988b7ecdce5
thuan.tran@bstarsolutions.com <thuan.tran@bstarsolutions.com>
2025-09-30 10:51:42 +0700

    # handle app , server files

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require("./src/app");
+const PORT = 3055;
+const server = app.listen(PORT, () => {
+  console.log(`WSV E.Coffee start with port::${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Exit server Express !!");
+    //--notify send ....PING
+  });
+});
