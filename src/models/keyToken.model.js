bdca0ff6945496d6931059f1ea4fbcc72d3c7c3e
thuan.tran@bstarsolutions.com <thuan.tran@bstarsolutions.com>
2025-10-03 17:25:19 +0700

    #6 handle dynamic middleware check api key and permission

--- src/models/keyToken.model.js
+++ src/models/keyToken.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose"); // Erase if already required
 const DOCUMENT_NAME = "Key";
 const COLLECTION_NAME = "Keys";
 // Declare the Schema of the Mongo model
-var keyTokenSchema = new Schema(
+const keyTokenSchema = new Schema(
   {
     user: {
       type: Schema.Types.ObjectId,
---
+++ src/services/apiKey.service.js
@@ -0,0 +1,21 @@
+// @ts-nocheck
+"use strict";
+
+const apiKeyModel = require("../models/apiKey.model");
+const crypto = require("crypto");
+
+const findById = async (key) => {
+  //--fake create api key
+  //   const newKey = await apiKeyModel.create({
+  //     key: crypto.randomBytes(64).toString("hex"),
+  //     permissions: ["0000"],
+  //   });
+  //   console.log("--key::", newKey);
+
+  const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
+  return objKey;
+};
+
+module.exports = {
+  findById,
+};
